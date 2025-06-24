1d4a4c8cb95703e8022ddf1160b4d1856ed1e4a2
Ysaragon <Ysaragon95@gmail.com>
2025-06-24 11:52:47 -0500

    Add UnitTest Project with mstest

---
+++ test/ToDo.Test/ToDo.Domain/Entities/Security/UserTests.cs
@@ -0,0 +1,18 @@
+﻿using ToDo.Domain.Entities.Security;
+
+namespace ToDo.Test.ToDo.Domain.Entities.Security
+{
+    [TestClass]
+    public class UserTest
+    {
+        [TestMethod]
+        public void User_HasDefaultValues()
+        {
+            // Act
+            User user = new User();
+
+            // Assert
+            Assert.AreEqual(null, user.Name);
+        }
+    }
+}
